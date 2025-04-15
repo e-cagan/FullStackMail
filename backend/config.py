301ed67d55afb0e60e4f5aa33d2aa045f583ec54
e-cagan <emincaganapaydin@gmail.com>
2025-04-15 23:54:37 +0300

    First version

---
+++ backend/config.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = os.urandom(24).hex()
+
+app.config["SESSION_COOKIE_HTTPONLY"] = True
+app.config["SESSION_COOKIE_SECURE"] = False  # Development için False, production için True
+app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
+
+db = SQLAlchemy(app)
