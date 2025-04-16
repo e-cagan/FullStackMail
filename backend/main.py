293445f833afc8c4c35ed6ad143feff854b8d177
e-cagan <emincaganapaydin@gmail.com>
2025-04-16 18:12:09 +0300

    Updating port.

--- backend/main.py
+++ backend/main.py
@@ -314,4 +314,4 @@ def email_action(email_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
