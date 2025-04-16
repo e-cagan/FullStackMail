55e036b5d88fdc95795f8b2a8e8775f07ff90ff0
e-cagan <emincaganapaydin@gmail.com>
2025-04-16 09:46:36 +0300

    Add SECRET_KEY from .env and update config

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,7 @@ CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = os.urandom(24).hex()
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev_default_key")
 
 app.config["SESSION_COOKIE_HTTPONLY"] = True
 app.config["SESSION_COOKIE_SECURE"] = False  # Development için False, production için True
