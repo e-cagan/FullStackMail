161c2477af5f1ff9bef994b753863dc9101e4df4
e-cagan <emincaganapaydin@gmail.com>
2025-04-16 15:03:37 +0300

    Fixing CORS error.

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=["http://localhost:3000", "https://ismailspam.onrender.com"])
+CORS(app, supports_credentials=True, origins=["http://localhost:3000", "https://full-stack-mail.vercel.app"])
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
