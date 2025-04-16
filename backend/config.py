c9f227a66fc5f8f18a91e576a400d061270b64de
e-cagan <emincaganapaydin@gmail.com>
2025-04-16 17:34:23 +0300

    Changing secure cookie to true.

--- backend/config.py
+++ backend/config.py
@@ -11,7 +11,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev_default_key")
 
 app.config["SESSION_COOKIE_HTTPONLY"] = True
-app.config["SESSION_COOKIE_SECURE"] = False  # Development için False, production için True
+app.config["SESSION_COOKIE_SECURE"] = True  # Development için False, production için True
 app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
 
 db = SQLAlchemy(app)
